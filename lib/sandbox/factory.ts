5b34bbfcc11e2c51cd0a601e33956ebd5782370d
Sam Sammane <sam@sammane.com>
2026-04-12 04:39:53 +0000

    Revert "refactor(sandbox): docker-only factory"
    This reverts commit 1226fe149c27f5c9028d300d04c4885e5b77ad5f.

--- lib/sandbox/factory.ts
+++ lib/sandbox/factory.ts
@@ -1,11 +1,21 @@
 import type { SandboxProvider } from './provider'
-import { DockerSandboxProvider } from './providers/docker'
 
 let cachedProvider: SandboxProvider | null = null
 
 export function getSandboxProvider(): SandboxProvider {
-  if (!cachedProvider) {
-    cachedProvider = new DockerSandboxProvider()
+  if (cachedProvider) return cachedProvider
+
+  const providerType = process.env.SANDBOX_PROVIDER || (process.env.SANDBOX_SSH_HOST ? 'docker' : 'vercel')
+
+  if (providerType === 'docker') {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const mod = require(/* webpackIgnore: true */ './providers/docker')
+    cachedProvider = new mod.DockerSandboxProvider()
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const mod = require(/* webpackIgnore: true */ './providers/vercel')
+    cachedProvider = new mod.VercelSandboxProvider()
   }
-  return cachedProvider
+
+  return cachedProvider!
 }
